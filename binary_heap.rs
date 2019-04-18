55ed062fe2e503d5061889caf312e6f67b5c887f
Ilya fanyShu <ilyashvy@gmail.com>
2019-04-18 10:54:53 +0300

    heap refacctor

--- binary_heap.rs
+++ binary_heap.rs
@@ -1,100 +1,145 @@
 #[derive(Debug)]
-struct BHeap {
-  list: Vec<usize>
+struct Heap {
+  nodes: Vec<u64>
 }
 
-impl BHeap {
-  fn new() -> Self {
-    BHeap { list: Vec::new() }
+impl Heap {
+  pub fn new() -> Self {
+    Heap { nodes: vec![0] }
   }
 
-  fn swap_up(&mut self, index: Option<usize>) {
-    let mut i = self.list.len() - 1;
-    if let Some(idx) = index {
-      i = idx;
-    }
+  pub fn insert(&mut self, value: u64) {
+    let lastIndex = self.nodes.len();
+    self.nodes.push(value);
+    self.siftUp(lastIndex);
+  }
 
-    let mut parent = i / 2;
+  pub fn extract_max(&mut self) -> u64 {
+    let firstIndex = 1;
+    let lastIndex = self.nodes.len() - 1;
+    let max = self.nodes[firstIndex];
+    self.nodes.swap(firstIndex, lastIndex);
+    self.nodes.pop();
+    if self.nodes.len() == 0 { return max; }
+    self.siftDown(firstIndex);
+    max
+  }
 
-    while i > 0 && self.list[parent] < self.list[i] {
-      self.list.swap(i, parent);
+  fn siftUp(&mut self, index: usize) {
+    let mut i = index;
+    let mut parent = self.parent(&i);
+    while parent >= 1 {
+      if self.nodes[parent] < self.nodes[i] {
+        self.nodes.swap(parent, i);
+      }
+
       i = parent;
-      parent = i / 2;
+      parent = self.parent(&i);
     }
   }
 
-  fn swap_down(&mut self) {
-    let mut i = 0;
-    let mut left_child = (i * 2) + 1;
-    let mut right_child = (i * 2) + 2;
+  fn siftDown(&mut self, index: usize) {
+    let mut left = self.left(&index);
+    let mut right = self.right(&index);
+    let mut i = index;
+    let mut nextIndex = 0;
 
-    loop {
-      if left_child > self.list.len() - 1 || right_child > self.list.len() - 1 {
-        break
+    while left < self.nodes.len() {
+      if right < self.nodes.len() && self.nodes[right] > self.nodes[left] {
+        nextIndex = right;
+      } else {
+        nextIndex = left;
       }
 
-      if self.list[left_child] > self.list[right_child] && self.list[i] < self.list[left_child] {
-        self.list.swap(left_child, i);
-        i = left_child;
-      } else if self.list[i] < self.list[right_child] {
-        self.list.swap(right_child, i);
-        i = right_child;
+      if self.nodes[i] < self.nodes[nextIndex] {
+        self.nodes.swap(i, nextIndex);
+        i = nextIndex;
+        left = self.left(&i);
+        right = self.right(&i);
       } else { break }
-
-      left_child = (i * 2) + 1;
-      right_child = (i * 2) + 2;
     }
   }
 
-  fn insert(&mut self, value: usize) {
-    self.list.push(value);
-    self.swap_up(None);
+  fn left(&self, index: &usize) -> usize {
+    index * 2
   }
 
-  fn max_heapify(&mut self) -> usize {
-    let last_node = self.list.len() - 1;
-    self.list.swap(0, last_node);
-    let max_node = self.list[last_node];
-    self.list.pop();
-    self.swap_down();
-    max_node
+  fn right(&self, index: &usize) -> usize {
+    (index * 2) + 1
   }
 
-  fn remove(&mut self, value: usize) {
-    match self.list.iter().position(|el| el == &value) {
-      None => panic!("element not found"),
-      Some(index) => {
-        self.list[index] = 9999;
-        self.swap_up(Some(index));
-        self.max_heapify();
-      }
-    };
-  }
-
-  fn change_priority(&mut self, from: usize, to: usize) {
-    match self.list.iter().position(|el| el == &from) {
-      None => panic!("element not found"),
-      Some(index) => {
-        self.list[index] = to;
-        self.swap_up(Some(index));
-        self.swap_down();
-      }
-    };
+  fn parent(&self, index: &usize) -> usize {
+    index / 2
   }
 }
 
 fn main() {
-  let mut sss = BHeap::new();
-  sss.insert(11);
-  sss.insert(10);
-  sss.insert(5);
-  sss.insert(2);
-  sss.insert(1);
-  sss.insert(6);
-  sss.insert(7);
-  sss.insert(8);
-  sss.insert(3);
-  sss.insert(4);
-  sss.change_priority(7, 2);
-  dbg!(sss);
+  let mut heap = Heap::new();
+  heap.insert(304);
+  heap.insert(255);
+  heap.insert(146);
+  heap.insert(29);
+  heap.insert(157);
+  heap.insert(96);
+  heap.insert(105);
+  heap.extract_max();
+  heap.insert(200);
+  heap.insert(10);
+  heap.extract_max();
+  heap.insert(5);
+  heap.insert(500);
+  heap.extract_max();
+  heap.insert(10);
+  heap.insert(8);
+  heap.insert(10);
+  heap.insert(10);
+  heap.extract_max();
+  heap.extract_max();
+  heap.insert(2);
+  heap.insert(3);
+  heap.insert(15);
+  heap.insert(18);
+  heap.insert(12);
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.insert(3);
+  heap.insert(0);
+  heap.extract_max();
+  heap.extract_max();
+  heap.insert(32323);
+  heap.insert(334);
+  heap.insert(11111);
+  heap.extract_max();
+  heap.extract_max();
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.insert(323123123);
+  heap.insert(100000000);
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+  heap.extract_max();
+
+  println!("{:#?}", heap);
 }
